b3ff6995dbd435473bfb7cb89ba507ea0ddfdba2
sharmaprasid <prasidpoudelsharma@gmail.com>
2023-09-23 17:01:45 +0545

    updated

--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -1,34 +1,39 @@
 import mongoose from "mongoose";
 
-const userSchema=new mongoose.Schema({
-    id:{
-        type:String,
-        required:true
+const userSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: String,
+  bio: String,
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread",
     },
-    username:{
-        type:String,
-        required:true,
-        unique:true
+  ],
+  onboarded: {
+    type: Boolean,
+    default: false,
+  },
+  communities: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Community",
     },
-    name:{
-        type:String,
-        required:true
-    },
-    image:String,
-    bio:String,
-    threads:[{
-        types:mongoose.Types.ObjectId,
-        ref:'Thread'
-    }],
-    onboarded:{
-        type:Boolean,
-        default:false
-    },
-    communities:[{
-        types:mongoose.Types.ObjectId,
-        ref:'Community'
-    }],
+  ],
+});
 
-})
-const User=mongoose.models.User||mongoose.model("User",userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
 export default User;
