50a80518807ed236f3d54aaa4334f8a0cfffee59
sharmaprasid <prasidpoudelsharma@gmail.com>
2023-09-23 17:58:11 +0545

    error fixed

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,41 +1,65 @@
-"use client"
-import Link from "next/link"
-import { sidebarLinks } from "@/constants/index"
-import Image from "next/image"
-import { usePathname, useRouter } from "next/navigation"
-import { SignedIn, SignOutButton, OrganizationSwitcher } from "@clerk/nextjs"
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
+
+import { sidebarLinks } from "@/constants";
 
 const LeftSidebar = () => {
     const router = useRouter();
     const pathname = usePathname();
-    return (
-        <section className="custom-scrollbar leftsidebar">
-            <div className="flex w-full flex-1 flex-col gap-6 px-6">
-                {
-                    sidebarLinks.map((links) => {
-                        const isActive = (pathname.includes(links.route) && links.route.length > 1) || pathname == links.route;
-                        return (
 
-                            <Link href={links.route} key={links.label} className={`leftsidebar_link ${isActive && 'bg-primary-500'}`} >
-                                <Image src={links.imgURL} alt={links.label} width={24} height={24} />
-                                <p className="text-light-1 max-lg:hidden">{links.label}</p>
-                            </Link>
-                        )
-                    })
-                }
+    const { userId } = useAuth();
+
+    return (
+        <section className='custom-scrollbar leftsidebar'>
+            <div className='flex w-full flex-1 flex-col gap-6 px-6'>
+                {sidebarLinks.map((link) => {
+                    const isActive =
+                        (pathname.includes(link.route) && link.route.length > 1) ||
+                        pathname === link.route;
+
+                    if (link.route === "/profile") link.route = `${link.route}/${userId}`;
+
+                    return (
+                        <Link
+                            href={link.route}
+                            key={link.label}
+                            className={`leftsidebar_link ${isActive && "bg-primary-500 "}`}
+                        >
+                            <Image
+                                src={link.imgURL}
+                                alt={link.label}
+                                width={24}
+                                height={24}
+                            />
+
+                            <p className='text-light-1 max-lg:hidden'>{link.label}</p>
+                        </Link>
+                    );
+                })}
             </div>
-            <div className="mt-10 px-6">
+
+            <div className='mt-10 px-6'>
                 <SignedIn>
-                    <SignOutButton signOutCallback={() => { router.push('/sign-in') }}>
-                        <div className="flex cursor-pointer gap-4 p-4">
-                            <Image src='/assets/logout.svg' alt='logout' width={24} height={24} />
-                            <p className="text-light-2 max-lg:hidden">Logout</p>
+                    <SignOutButton signOutCallback={() => router.push("/sign-in")}>
+                        <div className='flex cursor-pointer gap-4 p-4'>
+                            <Image
+                                src='/assets/logout.svg'
+                                alt='logout'
+                                width={24}
+                                height={24}
+                            />
+
+                            <p className='text-light-2 max-lg:hidden'>Logout</p>
                         </div>
                     </SignOutButton>
                 </SignedIn>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default LeftSidebar
+export default LeftSidebar;
